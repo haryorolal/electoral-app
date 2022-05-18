db4fe331313174a204266edb8818a536f7a525d0
Ayodele Olaleye <haryorolal2@gmail.com>
2022-05-18 21:56:52 +0100

    new one

--- src/app/pages/auth/auth-routing.module.ts
+++ src/app/pages/auth/auth-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { UnauthGuard } from "src/app/guards";
+import { AuthGuard, UnauthGuard } from "src/app/guards";
 
 const route: Routes = [
     {path: 'login', loadChildren: () => import('./component/login/login.module').then(x => x.LoginModule),
@@ -9,7 +9,9 @@ const route: Routes = [
     {path: 'registration', loadChildren: () => import('./component/registration/registration.module').then(x => x.RegistrationModule),
     canActivate: [UnauthGuard]
     },
-    {path: 'email-confirm', loadChildren: () => import('./component/email-confirm/email-confirm.module').then(x => x.EmailConfirmModule)},
+    {path: 'email-confirm', loadChildren: () => import('./component/email-confirm/email-confirm.module').then(x => x.EmailConfirmModule),
+    canActivate: [AuthGuard]
+    },
     {path: '**', pathMatch: 'full', redirectTo: 'login'}
 ];
 
